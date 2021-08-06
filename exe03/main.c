bbacc2a7590e380214963acf8d353259c64c469a
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-05 22:11:11 -0300

    Update main.c

--- exe03/main.c
+++ exe03/main.c
@@ -1,8 +1,43 @@
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
+
+// Bibliotecas
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main (void)
+{
+	// Variáveis
+	int n;
+   
+	// Entradas
+	scanf("%d",&n);
+   
+	// Processamento e saída
+	if(n % 2 == 0)
+   {
+      if(n < 0)
+      {
+         printf("NUMERO PAR NEGATIVO");
+      }
+      else if(n == 0)
+      {
+         printf("NUMERO NEUTRO");
+      }
+      else
+      {
+         printf("NUMERO PAR POSITIVO");
+      }
+   }
+   else
+   {
+      if(n < 0)
+      {
+         printf("NUMERO IMPAR NEGATIVO");
+      }
+      else
+      {
+         printf("NUMERO IMPAR POSITIVO");
+      }
+   }
 }
