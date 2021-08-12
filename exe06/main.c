b5716ca0c5297e3ecd525454b75c65b309b2345c
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-12 10:31:26 -0300

    Update main.c

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,36 @@
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
+
+// Bibliotecas
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main (void)
+{
+	// Variáveis
+	int x, y, z;
+	
+	// Leitura
+	scanf("%d %d %d",&x,&y,&z);
+	
+	// Processamento e saída
+	if(x < y + z || y < x + z || z < x + y)
+	{
+		if(x == y == z)
+		{
+			printf("TRIANGULO EQUILÁTERO\n");
+		}
+		else if(x == y || y == z || z == x)
+		{
+			printf("TRIANGULO ISÓSCELES\n");
+		}
+		else
+		{
+			printf("TRIANGULO ESCALENO\n");
+		}
+	}
+	else
+	{
+		printf("OS LADOS NAO FORMAM UM TRIANGULO\n");
+	}
 }
