f20304af0a65cfe400f418ecb051965d1ddfbd75
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-12 10:30:28 -0300

    Update main.c

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,42 @@
-#include <stdio.h>
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+// Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	// Variáveis
+	int menor = 0;
+	int maior = 0;
+	int vetor* = (int*)malloc(sizeof(int)*3);
+	
+	// Entrada
+	for(int i = 0; i < 3; i++)
+	{
+		scanf("%d", vetor[i]);
+		if(i == 0)
+		{
+			maior = vetor[i];
+			menor = vetor[i];
+		}
+		
+		if(vetor[i] > maior)
+		{
+			maior = vetor[i];
+		}
+		else
+		{
+			if(vetor[i] < menor)
+			{
+				menor = vetor[i];
+			}
+		}
+	}
+	
+	// Saída
+	printf("MAIOR = %d", &maior);
+	printf("MENOR = %d", &menor);
 }
