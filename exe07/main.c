888446ebcd0c9925b2fd5af33bf14d3a34b3c6f5
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-12 20:07:01 -0300

    Update main.c

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,42 @@
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
+
+// Bibliotecas
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+// Main
+int main (void)
+{
+	int N;
+	int maior;
+	int menor;
+	
+	scanf("%d", &N);
+	
+	int vetor[N];
+	for(int i = 0; i < N; i++)
+	{
+		scanf("%d", &vetor[i]);
+		if(i == 0)
+		{
+			maior = vetor[i];
+			menor = vetor[i];
+		}
+		
+		if(vetor[i] > maior)
+		{
+			maior = vetor[i];
+		}
+		else
+		{
+			if(vetor[i] < menor)
+			{
+				menor = vetor[i];
+			}
+		}
+	}
+	
+	printf("MAIOR = %d\n",maior);
+	printf("MENOR = %d\n",menor);
 }
