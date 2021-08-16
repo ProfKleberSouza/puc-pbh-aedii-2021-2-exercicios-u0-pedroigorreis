a423c6d97a2d469f66c69a82af2b37ad360532e0
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-16 08:42:01 -0300

    Update main.c

--- exe10/main.c
+++ exe10/main.c
@@ -1,8 +1,52 @@
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
+
+// Bibliotecas
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+// Ordenação inversa
+void bubbleSort(int vetor[], int n)
+{
+	int k;
+	int j;
+	int aux;
+	
+	for(k = 1; k < n; k++)
+	{
+		for(j = 0; j < n - 1;j++)
+		{
+			if(vetor[j] < vetor[j+1])
+			{
+				aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+			}
+		}
+	}
+}
+
+// Main
+int main (void)
+{
+	// Variáveis
+	int n[10];
+	
+	// Leitura
+	for(int i = 0; i < 10; i++)
+	{
+		scanf("%d", &n[i]);
+	}
+	
+	// Ordenação inversa
+	bubbleSort(n, 10);
+	
+	// Impressão
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%d\n", n[i]);
+	}
+	
+	// Fim
+	return 0;
 }
