3a8ee0954d4cc82b97b2c32161363418975bb421
Pedro Igor <88516388+pedroigorreis@users.noreply.github.com>
2021-08-26 20:52:08 +0000

    Fixing Exe09

--- exe09/main.c
+++ exe09/main.c
@@ -1,8 +1,47 @@
-#include <stdio.h>
+// +-------------------------------+----------------+
+// | U0 - Nivelamento - Exercícios | @pedroigorreis |
+// +-------------------------------+----------------+
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+// Bibliotecas
+#include <stdio.h>
+#include <string.h>
+
+// Limites
+#define MAX 30
+
+// Main
+int main (void)
+{
+  // Quantidade de leituras
+  int N;
+  int i;
+  scanf("%d", &N);
+
+  // Vetores
+  float notas[N];
+  char nomes[N][MAX];
+
+  // Leituras
+  for(i = 0; i < N; i++)
+  {
+    scanf("%s %f", nomes[i], &notas[i]);
+  }
+
+  // Média
+  float soma = 0;
+  float media = 0;
+  for(int i = 0; i < N; i++)
+  {
+    soma = notas[i] + soma;
+  }
+  media = (soma)/N;
+
+  // Impressão
+  printf("NOTA MEDIA = %.1f\n", media);
+
+  // Ordenar alafabeticamente
+  for(i = 0; i < N; i++)
+  {
+    printf("%s %1.f\n",nomes[i],notas[i]);
+  }
 }
